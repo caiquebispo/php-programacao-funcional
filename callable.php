776214bfffb9fb499cd0d99d35495b5c22a2c421
Caique Bispo <caiquebispo0023@gmail.com>
2024-09-15 13:49:08 -0300

    feat: Mapeando dados

--- callable.php
+++ callable.php
@@ -2,6 +2,12 @@
 
 $dados = require 'dados.php';
 
-$contador = count($dados);
+function convertPaisParaLetraMaiuscaula(array $pais): array
+{
+    $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
+    return $pais;
+}
 
-echo "Número de paises: {$contador} \n";
+$dados = array_map('convertPaisParaLetraMaiuscaula', $dados);
+
+var_dump($dados);
