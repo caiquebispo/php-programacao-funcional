0f4f5475e14f981a3233042ed66f60358ad27d9f
Caique Bispo <caiquebispo0023@gmail.com>
2024-09-15 13:36:02 -0300

    feat: Contando Paises

--- callable.php
+++ callable.php
@@ -1,15 +1,7 @@
 <?php
 
-$variavel = "Isso é uma variavel que está no escopo exeterno do programa";
+$dados = require 'dados.php';
 
-function outraFuncao(callable $fn)
-{
+$contador = count($dados);
 
-    echo "Minha funcao princial";
-    echo $fn();
-}
-
-outraFuncao(function () use ($variavel) {
-
-    echo "Olá, eu estou usando uma variavél que está no contexto externo da minha aplicação por meio de uma closure " . $variavel;
-});
+echo "Número de paises: {$contador} \n";
