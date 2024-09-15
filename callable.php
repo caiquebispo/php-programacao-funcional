9baa97caa10dcaae37a032ad8ea73fe7761ac577
Caique Bispo <caiquebispo0023@gmail.com>
2024-09-15 13:57:24 -0300

    feat: Filtrando dados

--- callable.php
+++ callable.php
@@ -7,7 +7,13 @@ function convertPaisParaLetraMaiuscaula(array $pais): array
     $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
     return $pais;
 }
+function verificaSePaisTemEspacoNoNome(array $pais): bool
+{
 
+    return str_contains($pais['pais'], ' ');
+}
 $dados = array_map('convertPaisParaLetraMaiuscaula', $dados);
+
+$dados = array_filter($dados, 'verificaSePaisTemEspacoNoNome');
 
 var_dump($dados);
